ca87722bfe16f366b7b0940bdcc7d2b6d38fdf0f
Simon <simon.dickson@pebblecode.com>
2013-10-09 09:24:38 +0100

    Intial commit

---
+++ AuthorizationFSharp/Program.fs
@@ -0,0 +1,38 @@
+﻿open System
+open System.Text
+open ZeroMQ
+
+module Zmq =
+    let responder addresss (zmqContext:ZmqContext) =
+        let responder = zmqContext.CreateSocket SocketType.REP
+        responder.Bind addresss
+        responder
+
+module Server =
+    let start requestProcessor (recieveSocket:ZmqSocket) =
+        async {
+            while true do
+                let request = recieveSocket.Receive Encoding.UTF8
+                let! response = requestProcessor request
+                recieveSocket.Send(response, Encoding.UTF8) |> ignore
+        }
+
+let isAuthorized (message:string) =
+    async {
+        printfn "Recieved message: %s" message
+        let x = message.Split ','
+        if x.[0] = "Bob" && x.[1] = "1" then
+            return true.ToString()
+        else
+            return false.ToString()
+    }
+
+[<EntryPoint>]
+let main argv =
+    ZmqContext.Create()
+    |> Zmq.responder "tcp://*:5556"
+    |> Server.start isAuthorized
+    |> Async.Start
+
+    Console.ReadLine() |> ignore
+    0
