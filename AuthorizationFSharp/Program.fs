bed1df0eb2cb82ae28d072f4d73e759e6031b58d
Simon <simon.dickson@pebblecode.com>
2013-10-10 08:57:37 +0100

    Changes router to be fully load balancing

--- AuthorizationFSharp/Program.fs
+++ AuthorizationFSharp/Program.fs
@@ -1,4 +1,5 @@
 ﻿open System
+open System.Collections.Generic
 open System.Threading
 open System.Text
 open ZeroMQ
@@ -6,41 +7,50 @@ open ZeroMQ
 module Zmq =
     let router frontend backend  (zmqContext:ZmqContext) =
         let router = zmqContext.CreateSocket SocketType.ROUTER
-        let dealer = zmqContext.CreateSocket SocketType.DEALER
+        let dealer = zmqContext.CreateSocket SocketType.ROUTER
         router.Bind frontend
         dealer.Bind backend
         (router, dealer)
 
-    let dealer address (zmqContext:ZmqContext) =
-        let dealer = zmqContext.CreateSocket SocketType.DEALER
+    let worker address (zmqContext:ZmqContext) =
+        let dealer = zmqContext.CreateSocket SocketType.REQ
         dealer.Connect address
         dealer
 
 module Server =
-    let routeMessages (routerFrom:ZmqSocket) (routerTo:ZmqSocket) _ =
+    let routeWorkerMessages (routerFrom:ZmqSocket) (routerTo:ZmqSocket) (queue:Queue<Frame>) _ =
         let message = routerFrom.ReceiveMessage()
+        queue.Enqueue(message.Unwrap())
+        if Encoding.UTF8.GetString(message.[0].Buffer) <> "Ready" then
+            routerTo.SendMessage(message) |> ignore
+
+    let routeClientMessages (routerFrom:ZmqSocket) (routerTo:ZmqSocket) (queue:Queue<Frame>) _ =
+        let message = routerFrom.ReceiveMessage()
+        message.Wrap(queue.Dequeue())
         routerTo.SendMessage(message) |> ignore
 
     let startRouter (frontEnd:ZmqSocket, backEnd:ZmqSocket) =
         async {
-            frontEnd.ReceiveReady.Add (routeMessages frontEnd backEnd)
-            backEnd.ReceiveReady.Add (routeMessages backEnd frontEnd)
-            let poller = new Poller([|frontEnd; backEnd|])
+            let queue = new Queue<Frame>()
+            frontEnd.ReceiveReady.Add (routeClientMessages frontEnd backEnd queue)
+            backEnd.ReceiveReady.Add (routeWorkerMessages backEnd frontEnd queue)
+            let poller = new Poller([| frontEnd; backEnd |])
             while true do
                 poller.Poll() |> ignore
         }
 
-    let createWorker requestProcessor (dealerSocket:ZmqSocket) =
+    let createWorker requestProcessor (workerSocket:ZmqSocket) =
         async {
+            workerSocket.Send("Ready", Encoding.UTF8) |> ignore
             while true do
-                let message = dealerSocket.ReceiveMessage()
+                let message = workerSocket.ReceiveMessage()
                 let respondTo = message.Unwrap()
                 let request = Encoding.UTF8.GetString(message.[0].Buffer)
                 let! (response:string) = requestProcessor request
                 let responseMessage = new ZmqMessage()
                 responseMessage.Push(new Frame(Encoding.UTF8.GetBytes(response)))
                 responseMessage.Wrap(respondTo)
-                dealerSocket.SendMessage(responseMessage) |> ignore
+                workerSocket.SendMessage(responseMessage) |> ignore
         }
 
 let isAuthorized (message:string) =
@@ -65,7 +75,7 @@ let main argv =
    
     for i in 1 .. 10 do
         context
-        |> Zmq.dealer "inproc://backend"
+        |> Zmq.worker "inproc://backend"
         |> Server.createWorker isAuthorized
         |> Async.Start
 
